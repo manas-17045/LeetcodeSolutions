3fbaf8473c64eccdf2dfcb8f3d3cc62632dde1a0
MANAS <mrmanasb@gmail.com>
2025-04-30 10:04:50 +0530

    Added documentation Solution 1 for Leetcode Problem 1295

--- Leetcode 1295/1295_1.py
+++ Leetcode 1295/1295_1.py
@@ -3,6 +3,17 @@
 
 class Solution:
     def findNumbers(self, nums: list[int]) -> int:
+        """
+        Finds the count of numbers with an even number of digits in the provided list.
+        The function iterates through the list, determines the number of digits
+        for each number, and checks if the number of digits is even. If so, it increments
+        the count. The final count of such numbers is returned.
+
+        :param nums: A list of integers to analyze.
+        :type nums: list[int]
+        :return: Number of integers with an even number of digits.
+        :rtype: int
+        """
         even_digit_count = 0
 
         # Iterate through each number in the input list
