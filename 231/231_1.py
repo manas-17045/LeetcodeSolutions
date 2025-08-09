ffa55e11f0a501221e4b6f071c4bb1371b1ec6c3
MANAS <mrmanasb@gmail.com>
2025-08-09 09:23:42 +0530

    Solution 1 for Leetcode 231 (Power of Two)

---
+++ 231/231_1.py
@@ -0,0 +1,16 @@
+# Leetcode 231: Power of Two
+# https://leetcode.com/problems/power-of-two/
+# Solved on 9th of August, 2025
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        """
+        Determines if a given integer is a power of two.
+
+        Args:
+            n (int): The integer to check.
+        Returns:
+            bool: True if n is a power of two, False otherwise.
+        """
+        isPositive = n > 0
+        hasOneBitSet = (n & (n - 1)) == 0
+        return isPositive and hasOneBitSet
