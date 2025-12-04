de808a2647b0864fa61324448f1f277ec412e5fe
MANAS <mrmanasb@gmail.com>
2025-12-04 23:47:15 +0530

    Python solution for Leetcode 2038 (Remove Colored Pieces if Both Neighbors are the Same Color)

--- Leetcode 2001-2100/2038/2038.py
+++ Leetcode 2001-2100/2038/2038.py
@@ -1,6 +1,6 @@
 # Leetcode 2038: Remove Colored Pieces if Both Neighbors are the Same Color
 # https://leetcode.com/problems/remove-colored-pieces-if-both-neighbors-are-the-same-color/
-# Solved on 4th of November, 2025
+# Solved on 4th of December, 2025
 class Solution:
     def winnerOfGame(self, colors: str) -> bool:
         """
