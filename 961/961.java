8259d906298a46c0f6a181f43cfe535e90310bef
MANAS <mrmanasb@gmail.com>
2026-01-02 10:09:17 +0530

    Java solution for Leetcode 961 (N-Repeated Element in Size 2N Array)

---
+++ 961/961.java
@@ -0,0 +1,20 @@
+// Leetcode 961: N-Repeated Element in Size 2N Array
+// https://leetcode.com/problems/n-repeated-element-in-size-2n-array/
+// Solved on 2nd of January, 2025
+class Solution {
+    /**
+     * Finds the element that appears N times in an array of size 2N.
+     * @param nums The input array of integers.
+     * @return The element that is repeated N times.
+     */
+    public int repeatedNTimes(int[] nums) {
+        for (int step = 1; step <= 3; step++) {
+            for (int i = 0; i < nums.length - step; i++) {
+                if (nums[i] == nums[i + step]) {
+                    return nums[i];
+                }
+            }
+        }
+        return -1;
+    }
+}
