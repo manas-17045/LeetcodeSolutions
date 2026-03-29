27f819f1f5323bac1e206bec3c94aa06a5bc4eb2
MANAS <mrmanasb@gmail.com>
2026-03-29 20:15:04 +0530

    Java solution for Leetcode 3875 (Construct Uniform Parity Array I)

---
+++ Leetcode 3801-3900/3875/3875.java
@@ -0,0 +1,13 @@
+// Leetcode 3875: Construct Uniform Parity Array I
+// https://leetcode.com/problems/construct-uniform-parity-array- i/
+// Solved on 29th of March, 2026
+class Solution {
+    /**
+     * Determines if the given array can be transformed into a uniform parity array.
+     * @param nums1 An array of integers to be evaluated.
+     * @return true if the array satisfies the uniform parity conditions, false otherwise.
+     */
+    public boolean uniformArray(int[] nums1) {
+        return true;
+    }
+}
