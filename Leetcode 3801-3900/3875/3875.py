c66a3bd5236b507227e0e70a7b466fd0b672749a
MANAS <mrmanasb@gmail.com>
2026-03-29 20:14:58 +0530

    Python solution for Leetcode 3875 (Construct Uniform Parity Array I)

---
+++ Leetcode 3801-3900/3875/3875.py
@@ -0,0 +1,12 @@
+# Leetcode 3875: Construct Uniform Parity Array I
+# https://leetcode.com/problems/construct-uniform-parity-array-i/
+# Solved on 29th of March, 2026
+class Solution:
+    def uniformArray(self, nums1: list[int]) -> bool:
+        """
+        Determines if a uniform parity array can be constructed from the given input.
+
+        :param nums1: A list of integers to evaluate.
+        :return: A boolean indicating whether the uniform parity condition is met.
+        """
+        return True
