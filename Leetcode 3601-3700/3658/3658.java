c2a811b695e720852b6cb4797ea79d142d493506
MANAS <mrmanasb@gmail.com>
2026-07-15 09:52:57 +0530

    Java solution for Leetcode 3658 (GCD of Odd and Even Sums)

---
+++ Leetcode 3601-3700/3658/3658.java
@@ -0,0 +1,13 @@
+// Leetcode 3658: GCD of Odd and Even Sums
+// https://leetcode.com/problems/gcd-of-odd-and-even-sums/
+// Solved on 15th of July, 2026
+class Solution {
+    /**
+     * Counts the number of pairs (i, j) such that 1 <= i < j <= n and gcd(sum_odd(i..j), sum_even(i..j)) is odd.
+     * @param n the upper bound for i and j
+     * @return the number of pairs (i, j) satisfying the condition
+     */
+    public int gcdOfOddEvenSums(int n) {
+        return n;
+    }
+}
